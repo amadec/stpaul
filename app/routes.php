47622af5dd10e1a60e07c801b43c58fc1398d593
14LAURENTA <alaurent56@outlook.fr>
2016-03-14 11:02:45 +0100

    Routes.php modified

--- app/routes.php
+++ app/routes.php
@@ -5,4 +5,14 @@ $app->get('/', function () use ($app) {
     $sejours = $app['dao.sejour']->findAll();
     return $app['twig']->render('index.html.twig', array('sejours' => $sejours));
 });
+
+
+$app->match('/simul', function ( Request $request) use ($app) {
+
+    $simul = new Simul();
+
+    return $app['twig']->render('simul.html.twig', array(
+        'simul' => $simul));
+})->bind('simul');
+
 ?>
