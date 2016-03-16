c5992ed0fe394b107856a36640a406c8ef7fd2a3
14madeca <antoinemadec29490@gmail.com>
2016-03-16 08:42:47 +0100

    ajout classe simul

--- app/app.php
+++ app/app.php
@@ -1,10 +1,9 @@
 <?php
 
-use Symfony\Component\Debug\ErrorHandler;
-use Symfony\Component\Debug\ExceptionHandler;
-
 // Register global error and exception handlers
+use Symfony\Component\Debug\ErrorHandler;
 ErrorHandler::register();
+use Symfony\Component\Debug\ExceptionHandler;
 ExceptionHandler::register();
 
 // Register service providers.
@@ -12,9 +11,20 @@ $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app['twig'] = $app->share($app->extend('twig', function(Twig_Environment $twig, $app) {
+    $twig->addExtension(new Twig_Extensions_Extension_Text());
+    return $twig;
+}));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+// $app->register(new Silex\Provider\SecurityServiceProvider());
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider());
+
 
 // Register services.
 $app['dao.sejour'] = $app->share(function ($app) {
     return new stpaul\DAO\SejourDAO($app['db']);
 });
+
 ?>
